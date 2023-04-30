6b804c788fe93642324f88a775dab04b9f899b83
Aleksey <meysonsib200189@mail.ru>
2023-04-30 15:09:33 +0700

    replaced RecyclerView.Adapter by ListAdapter used DiffUtil + dataBinding

--- app/src/main/java/com/bignerdranch/android/tasks/recyclerview/adapter/TaskItemAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/tasks/recyclerview/adapter/TaskItemAdapter.kt
@@ -9,28 +9,23 @@ import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.tasks.R
+import com.bignerdranch.android.tasks.databinding.TaskItemBinding
 import com.bignerdranch.android.tasks.recyclerview.TaskDiffItemCallback
 import com.bignerdranch.android.tasks.room.Task
 
 class TaskItemAdapter : ListAdapter<Task,TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
 
-    class TaskItemViewHolder(val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
-
-        val taskName = rootView.findViewById<TextView>(R.id.task_name)
-        val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
-        val imageEmoticon = rootView.findViewById<ImageView>(R.id.imageEmoticon)
+    class TaskItemViewHolder(val binding: TaskItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item,parent,false) as CardView
-                return TaskItemViewHolder(view)
+                val binding = TaskItemBinding.inflate(layoutInflater,parent,false)
+                return TaskItemViewHolder(binding)
             }
         }
         fun bind(item : Task){
-            taskName.text = item.taskName
-            taskDone.isChecked = item.taskDone
-            imageEmoticon.setImageResource(R.drawable.ic_baseline_insert_emoticon_24)
+            binding.task = item
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder =

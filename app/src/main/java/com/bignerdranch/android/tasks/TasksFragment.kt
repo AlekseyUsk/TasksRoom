49a5154e95419b595b2f8ef3f9149b9e48ee6e47
Aleksey <meysonsib200189@mail.ru>
2023-05-02 12:47:25 +0700

    RecyclerView реагируют на события нажатия.

--- app/src/main/java/com/bignerdranch/android/tasks/TasksFragment.kt
+++ app/src/main/java/com/bignerdranch/android/tasks/TasksFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -34,7 +35,9 @@ class TasksFragment : Fragment() {
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
 
-        val adapter = TaskItemAdapter()
+        val adapter = TaskItemAdapter{ taskId ->
+            Toast.makeText(context,"Нажата задача ${taskId}",Toast.LENGTH_SHORT).show()
+        }
         binding.tasksList.adapter = adapter
 
         viewModel.tasks.observe(viewLifecycleOwner, Observer {
--- app/src/main/java/com/bignerdranch/android/tasks/recyclerview/adapter/TaskItemAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/tasks/recyclerview/adapter/TaskItemAdapter.kt
@@ -2,37 +2,38 @@ package com.bignerdranch.android.tasks.recyclerview.adapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bignerdranch.android.tasks.R
 import com.bignerdranch.android.tasks.databinding.TaskItemBinding
 import com.bignerdranch.android.tasks.recyclerview.TaskDiffItemCallback
 import com.bignerdranch.android.tasks.room.Task
 
-class TaskItemAdapter : ListAdapter<Task,TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
+class TaskItemAdapter(val clickListener: (taskId: Long) -> Unit) :
+    ListAdapter<Task, TaskItemAdapter.TaskItemViewHolder>(TaskDiffItemCallback()) {
 
     class TaskItemViewHolder(val binding: TaskItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val binding = TaskItemBinding.inflate(layoutInflater,parent,false)
+                val binding = TaskItemBinding.inflate(layoutInflater, parent, false)
                 return TaskItemViewHolder(binding)
             }
         }
-        fun bind(item : Task){
+
+        fun bind(item: Task, clickListener: (taskId: Long) -> Unit) {
             binding.task = item
+            binding.root.setOnClickListener {
+                clickListener(item.taskId)
+            }
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder =
         TaskItemViewHolder.inflateFrom(parent)
 
-    override fun onBindViewHolder(holder : TaskItemViewHolder, position: Int) {
-    var item = getItem(position)
-        holder.bind(item)
+    override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
+        var item = getItem(position)
+        holder.bind(item, clickListener)
     }
 }
